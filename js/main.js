192cb76f760d4e65885a99c5cad7c2c8dee317ba
Khuram <knshaikh@gmail.com>
2017-06-09 18:33:16 -0600

    generate random colors from array

--- js/main.js
+++ js/main.js
@@ -7,26 +7,40 @@ var colors = [
   "rgb(255, 0, 255)"
 ];
 
-var squares = document.querySelectorAll(".square");
-var colorDisplay = document.getElementById("colorDisplay");
-var pickedColor = colors[3];
+var squares       = document.querySelectorAll(".square");
+var colorDisplay  = document.getElementById("colorDisplay");
+var pickedColor   = pickColor();
+var resultDisplay = document.querySelector("#result");
 
 colorDisplay.textContent = pickedColor;
-for (var i = 0; i < squares.length; i++) {
+squares.forEach(function(square, index) {
 
   //Add initial colors to squares
-  squares[i].style.backgroundColor = colors[i];
+  square.style.backgroundColor = colors[index];
 
   //Add click listeners to squares
-  squares[i].addEventListener("click", function() {
+  square.addEventListener("click", function() {
     //grab color of clicked square
     var clickedColor = this.style.backgroundColor;
     //compare to picked color
     if (clickedColor === pickedColor) {
-      alert("Correct!");
+      resultDisplay.innerText = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert("Wrong!");
+      this.style.backgroundColor = "#232323";
+      resultDisplay.textContent = "Try Again...";
     }
   });
+});
+
+function changeColors(color) {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+    squares[i].textContent = "Correct!";
+  }
 }
 
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
